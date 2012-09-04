4084c75a0017888bbdc6df9e0fb22ce21841d0e2
Robin <robin.tollisen@gmail.com>
2012-09-04 13:07:39 +0200

    Added  a function which doesn't need a starting dictionary as argument.

--- NaiveBayesianClassifier/DataReader.cs
+++ NaiveBayesianClassifier/DataReader.cs
@@ -42,8 +42,38 @@ namespace NaiveBayesianClassifier
             return dataSet;
         }
 
-        public static Dictionary<string, int> ReturnWordCountFromFile(string filePath, Dictionary<string, int> wordsInCategory)
+        // Reads words in a file and adds the count to an exsisting dictionary.
+        public static Dictionary<string, int> ReturnWordCountFromFile(string filePath, Dictionary<string, int> addToDictionary)
         {
+            string fileContent = File.ReadAllText(filePath);
+            Regex rgx = new Regex("[^a-zA-Z0-9-]");
+            fileContent = rgx.Replace(fileContent, ".");
+            string[] wordsInFile = fileContent.Split('.');
+
+            foreach (string word in wordsInFile)
+            {
+                if (addToDictionary.ContainsKey(word))
+                {
+                    addToDictionary[word] = addToDictionary[word] + 1;
+                }
+                else
+                {
+                    addToDictionary[word] = 1;
+                }
+            }
+
+            if (addToDictionary.ContainsKey(""))
+            {
+                addToDictionary.Remove("");
+            }
+
+            return addToDictionary;
+        }
+
+        // Same as previous function, except it starts with an empty dictionary.
+        public static Dictionary<string, int> ReturnWordCountFromFile(string filePath)
+        {
+            Dictionary<string, int> wordsInCategory = new Dictionary<string, int>();
             string fileContent = File.ReadAllText(filePath);
             Regex rgx = new Regex("[^a-zA-Z0-9-]");
             fileContent = rgx.Replace(fileContent, ".");
@@ -68,6 +98,5 @@ namespace NaiveBayesianClassifier
 
             return wordsInCategory;
         }
-
     }
 }
